0d25f643abe7c83ead93c436fed0a7bb48e3dabf
Sarthak-SethiNsd <sarthaksethi31@gmail.com>
2026-08-05 20:11:37 +0530

    solve: CodeChef Starters 250 Presentation problem

---
+++ CodeChef-Contests/Starters250_Presentation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class CodeChef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(20 - n);
+        sc.close();
+    }
+}
