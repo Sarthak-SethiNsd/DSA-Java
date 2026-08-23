94f72c4b4eb9f51dbdcc9cd380816e803437b2d2
Sarthak-SethiNsd <sarthaksethi31@gmail.com>
2026-08-23 19:23:23 +0530

    solve: LeetCode-141-Linked List Cycle-Floyd's Cycle Detection Algorithm

---
+++ Leetcode-Problems/L141_Linked_List_Cycle.java
@@ -0,0 +1,21 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+public class L141_Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
